e5a1d4037e06e00e7124c5ac35c59d71ee628054
Matthew Chhay <chhaymatt@gmail.com>
2022-09-29 17:06:35 +1000

    Added copy to clipboard for email

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+// Source: https://stackoverflow.com/questions/47931843/javascript-copy-to-clipboard-not-working and https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_copy_clipboard2
+
+
+function copyText() {
+    navigator.clipboard.writeText("chhaymatt@gmail.com");
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: chhaymatt@gmail.com";
+  }
+
+function outFunc() {
+var tooltip = document.getElementById("myTooltip");
+tooltip.innerHTML = "Copy to clipboard";
+}
+
